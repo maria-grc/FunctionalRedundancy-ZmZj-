c71b113b3df30fa31e28e95fb3f6cb9cc0dc95a9
Maria Garcia <mariagarcia@Marias-MacBook-Pro-7.local>
2024-11-22 10:01:24 -0800

    Initial commit of all fines

---
+++ library.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+library(vegan)
+library(grid)
+library(lattice)
+library(tibble)
+library(cowplot)
+library(gridExtra)
+library(nlme)
+library(lme4)#use for glmer command
+library(MuMIn)
+library(reshape2)
+library(openair)
+
+library(tidyverse)
+library(glmmTMB)
+library(DHARMa)
+library(broom) 
+library(broom.mixed)
+
+library(emmeans)
+##theme better
+
+theme_better <- theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+          axis.title = element_text(size = 14), axis.text=element_text(size=12),
+        text = element_text(color = "black"))
+
+remove_lab <- theme(axis.text.x=element_blank(), 
+                    axis.ticks.x=element_blank(), 
+                    axis.text.y=element_blank(), 
+                    axis.ticks.y=element_blank()) 
+
+library(labdsv)
